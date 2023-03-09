940f1840f518986c29a98a34af293fce829ed242
iqlimafasha <114552189+iqlimafasha@users.noreply.github.com>
2023-03-09 15:10:06 +0700

    praktikum 3

---
+++ Praktikum 3/for_looping.php
@@ -0,0 +1,12 @@
+<?php
+for($i = 1;$i < 10;$i++){
+echo "Hello SI07 <br>";
+}
+echo "<br>";
+
+$array = ['kucing','anjing','kuda','kapibara'];
+
+for($i = 0;$i < count($array);$i++) {
+    echo $array[$i] . "<br>";
+}
+
